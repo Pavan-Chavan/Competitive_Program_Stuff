762203b19771b24d240e4acb8e1bbc5d93857357
Pavan Chavan <69139294+ig-terminal@users.noreply.github.com>
2021-07-04 21:00:21 +0530

    Compare the triplets

---
+++ CompareTheTriplets.cpp
@@ -0,0 +1,130 @@
+/*Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Comparison points is the total points a person earned.
+
+Given a and b, determine their respective comparison points.
+
+//Title:- Compare The Triplets 
+//coder:- Pavan Chavan
+//Email:- pavan.21920161@viit.ac.in
+//date:- 2/7/2021
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+vector<int> compareTriplets(vector<int> a, vector<int> b)
+ {
+    //we need to write this function only
+    vector<int> result;
+    int bob=0,alice=0;
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]>b[i])
+        bob++;
+        else if(a[i]<b[i])
+        alice++;
+    }
+    result.push_back(bob);
+    result.push_back(alice);
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(3);
+
+    for (int i = 0; i < 3; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    string b_temp_temp;
+    getline(cin, b_temp_temp);
+
+    vector<string> b_temp = split(rtrim(b_temp_temp));
+
+    vector<int> b(3);
+
+    for (int i = 0; i < 3; i++) {
+        int b_item = stoi(b_temp[i]);
+
+        b[i] = b_item;
+    }
+
+    vector<int> result = compareTriplets(a, b);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
